b29987df17ba83539455f1e548fb5414f7b7776f
meghna7838 <meghnatandon7838@gmail.com>
2024-04-18 21:23:36 +0530

    Status and callback hell

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 const divEle = document.querySelector('.card-container');
 
-const request = new XMLHttpRequest();
 
+function displayCard(id){
+const request1 = new XMLHttpRequest();
 //Open let us interact with server
 //What kind of request--first parameter
 //Second parameter is URL.
-request.open('GET',`https://dummyjson.com/users/1`);
+request1.open('GET',`https://dummyjson.com/users/${id}`);
 //Sending it to server
-request.send();
+request1.send();
 //To display content when it is loaded
-request.addEventListener('load',function(){
-    console.log(typeof request.responseText);//String
+request1.addEventListener('load',function(){
+    if(request1.status === 404) return;
+    console.log(typeof this.responseText);//String
     //To convert to json
-    console.log(JSON.parse(this.responseText));
     const data  = JSON.parse(this.responseText);
+    console.log(data);
+    cardDesign(data,'beforeend');
 
-    const card=`<div class= "user-card">
-    <img src="http://robohash.org/hicveldicta.png" alt="Profile Image">
-    <h3>John</h3>
-    <h3>Doe</h3>
-    <p class="email">j,doe@gmail.com</p>
+    const req2 = new XMLHttpRequest();
+    req2.open('GET',`https://dummyjson.com/users/${id-1}`);
+    req2.send();
+    req2.addEventListener('load',function(){
+        if(req2.status ===404 )  return;
+        const data = JSON.parse(this.responseText);
+        cardDesign(data,'afterbegin','other');
+
+        const req3 = new XMLHttpRequest();
+        req3.open('GET',`https://dummyjson.com/users/${id+1}`);
+        req3.send();
+        req3.addEventListener('load',function(){
+            if(req3.status ===404 )  return;
+            const data = JSON.parse(this.responseText);
+            cardDesign(data,'beforeend','other')
+        })
+    });
+});
+
+
+}
+
+displayCard(2);
+
+function cardDesign(data,pos,classname)
+{
+    const card=`<div class= "user-card ${classname}">
+    <img src=${data.image} alt="Profile Image">
+    <h3>${data.firstName}</h3>
+    <h3>${data.lastName}</h3>
+    <p class="email">${data.email}</p>
     <button class = "btn">View Profile</button>
     </div>`
 
-    divEle.innerHTML = card;
-});
+    divEle.insertAdjacentHTML(pos,card);
+}
